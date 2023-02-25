a0c885a32b4e11fe67a75c4518ba210b839b1262
quang <ltb01628645546@gmail.com>
2023-02-25 21:18:51 +0700

    daaas

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import router from "./src/router/router"
+
+const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+
+const DB = 'mongodb://127.0.0.1:27017/dbTest';
+mongoose.connect(DB)
+.then(()=> console.log('DB Connected'))
+.catch(error => console.log('DB connection error:', error.message));
+
+app.use('/api',router);
+
+app.listen(PORT, ()=>{
+    console.log("App running on port:" + PORT)
+})
